a84ef3defe16061065b4e815c30b739a1853a8fb
Alex Dasneves <adasneves127@gmail.com>
2024-04-08 13:23:01 -0400

    Fixed Base Routes

--- src/routes/base_routes.py
+++ src/routes/base_routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import request, session, redirect, send_file
 from src.utils.templates import send_template
 from src.utils.email_utils import send_bug_report
+import datetime, time
 
 
 @app.route("/", methods=["GET"])
@@ -9,16 +10,19 @@ def get_root_index():
     return send_template("index.liquid")
 
 
-@app.route("/about/", methods=["GET"])
-def get_about_page():
-    return send_template("about.liquid")
-
-
-@app.route("/about/bug_report", methods=["POST"])
-def send_report():
-    send_bug_report(request.form, session["user"])
-    return redirect("/about/")
 
 @app.route("/robots.txt", methods=['GET'])
 def send_robots():
     return send_file("interface/private/robots.txt")
+
+
+@app.route('/favicon.ico', methods=['GET'])
+def send_favicon():
+    return send_file('interface/private/favicon.ico')
+
+@app.before_request
+def prevent_rapid_requests():
+    if request.method == "POST":
+        last_post = session.get('last_post', 0)
+        if last_post >= time.time() - 10:
+            raise Exception

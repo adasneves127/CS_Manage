aba28f89f788facd022832396d6b687004a24745
Alex Dasneves <adasneves127@gmail.com>
2024-04-08 13:20:00 -0400

    Created Context Manager for DB

--- src/utils/app_utils.py
+++ src/utils/app_utils.py
@@ -1,5 +1,14 @@
 from json import load
-
+from contextlib import contextmanager
+from src.utils.db_utils import connect
 
 def load_app_info() -> dict:
     return load(open("app_info.json", "r"))
+
+@contextmanager
+def db_connection():
+    connection = connect()
+    try:
+        yield connection
+    finally:
+        connection.close()

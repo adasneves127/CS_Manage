a75163bcfbdb04719dbcbba2c8ec5b18f85bd767
Alex Dasneves <adasneves127@gmail.com>
2024-04-11 20:02:47 -0400

    Add Database Init in Workflow

--- test_suite.py
+++ test_suite.py
@@ -1,8 +1,13 @@
 import pytest
-
+from src.utils.db_utils import db_connection
 
 def test_initial():
     assert 1 == 1
+
+
+def test_database_connection():
+    with db_connection() as conn:
+        conn.initDB()
 
 
 if __name__ == "__main__":
